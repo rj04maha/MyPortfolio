450079767bfaa7727996900d102c5aac4acad0d1
Rachael <rj04maha@siena.edu>
2019-12-19 12:19:41 -0500

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,43 +5,45 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="white">
-      <div className="ui container">
-        <div className="ui secondary menu">
-          <div className="item">
-            <Link to="/">
-              <img
-                className="ui mini image"
-                src="/images/rblue.PNG"
-                alt="r"
-              ></img>
-            </Link>
+      <div className="ui container stackable grid">
+        <div className="eleven wide column">
+          <div className="ui secondary menu">
+            <div className="item">
+              <Link to="/">
+                <img
+                  className="ui mini image"
+                  src="/images/rblue.PNG"
+                  alt="r"
+                ></img>
+              </Link>
+            </div>
+            <div className="menu">
+              <Link to="/" className="menu-link item">
+                Home
+              </Link>
+              <Link to="/about" className="menu-link item">
+                About
+              </Link>
+              <Link to="/projects" className="menu-link item">
+                Projects
+              </Link>
+              <Link to="/contact" className="menu-link item">
+                Contact
+              </Link>
+            </div>
           </div>
-          <div className="menu">
-            <Link to="/" className="menu-link item">
-              Home
-            </Link>
-            <Link to="/about" className="menu-link item">
-              About
-            </Link>
-            <Link to="/projects" className="menu-link item">
-              Projects
-            </Link>
-            <Link to="/contact" className="menu-link item">
-              Contact
-            </Link>
-          </div>
-          <div className="right menu">
-            <a
-              href="https://docs.google.com/document/d/1wuyXiWsVhIcU-THH8aM_E0x3rEPYyPAOX1H5XiN9xy8/edit?usp=sharing"
-              className="no-hover item"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="ui button resume-button">
-                Download My Resume<i class="right chevron icon"></i>
-              </button>
-            </a>
-          </div>
+        </div>
+        <div className="five wide column">
+          <a
+            href="https://docs.google.com/document/d/1wuyXiWsVhIcU-THH8aM_E0x3rEPYyPAOX1H5XiN9xy8/edit?usp=sharing"
+            className="no-hover item"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="ui button resume-button">
+              Download My Resume<i class="right chevron icon"></i>
+            </button>
+          </a>
         </div>
       </div>
     </div>
